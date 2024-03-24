fba1ec8dfd144948d9375caa0ef8aed645475f39
ZAAFHachemrachid <rachidzaaf9@gmail.com>
2024-03-24 22:38:15 +0100

    hello

--- src/lib/projecte/WEBapp.svelte
+++ src/lib/projecte/WEBapp.svelte
@@ -4,7 +4,8 @@
 
 <div class="card w-80 mx-auto">
 	<img src={image} alt="API" class="" />
-	<div class="p-4 space-y-6">
+	<section class="p-4 space-y-6">
+	
 		<div class="flex justify-between items-center">
 			<b>WEB App</b>
 			<div class="text-sm opacity-70">55% completed</div>
@@ -12,7 +13,7 @@
 		<div class="text-sm opacity-70">
             I use svelte as a front-end framework to build the web app. I use Tailwind CSS to style the web app. I use Vite as a build tool. I use Vercel to deploy the web app.
         </div>
-	</div>
+	</section>
 	<!-- <hr /> -->
 	<footer class="p-4 border-t border-surface-300-600-token">
 		<div class="flex justify-between">
