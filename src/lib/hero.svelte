efbd1fb1edeceb5bc20f8f76d8efb6e8244cb27c
ZAAFHachemrachid <rachidzaaf9@gmail.com>
2024-03-25 23:46:14 +0100

    Update social media icons in hero component

--- src/lib/hero.svelte
+++ src/lib/hero.svelte
@@ -38,9 +38,9 @@
 				</li>
 			</ul>
 			<div class="flex flex-wrap gap-4">
-                <a href="https://t.me/raraeded09"  class="btn w-full md:w-fit variant-filled-primary md:btn-xl" ><Icon icon="icon-park-outline:telegram" /></a>
+                <a href="https://t.me/raraeded09"  class="btn w-full md:w-fit variant-filled-primary md:btn-xl" ><Icon icon="ic:sharp-telegram" width="24" height="24"  style="color: white" /></a>
 
-				<a href="https://www.instagram.com/raraeded?igsh=MW10eDdkZWM0aGc5Mg==" class="btn md:btn-lg w-full md:w-fit variant-filled-surface"><Icon icon="ion:logo-instagram" /></a>
+				<a href="https://www.instagram.com/raraeded?igsh=MW10eDdkZWM0aGc5Mg==" class="btn md:btn-lg w-full md:w-fit variant-filled-surface"><Icon icon="mdi:instagram" width="24" height="24"  style="color: black" /></a>
 			</div>
 		</div>
 
